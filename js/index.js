c1f7613ffc65751723a9874a0ebf9770a01d3f79
Jo Allam <joallam2008@gmail.com>
2025-08-16 00:57:05 +0300

    Arrivals and Reviews section

--- js/index.js
+++ js/index.js
@@ -17,4 +17,52 @@ document.addEventListener('DOMContentLoaded', async() => {
         img.setAttribute('src', data.results[index].picture.large);
         index++;
     })
-})
+
+    const scroller = document.querySelector("#arrivals");
+    const scrollerInner = scroller.querySelector("#slider");
+    const scrollerContent = Array.from(scrollerInner.children);
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `#slider`
+    scrollerContent.forEach((item) => {
+    const duplicatedItem = item.cloneNode(true);
+    duplicatedItem.setAttribute("aria-hidden", true);
+    scrollerInner.appendChild(duplicatedItem);
+    });
+
+
+    const track = document.querySelector('#reviews');
+    const nextBtn = document.querySelector('#next');
+    const prevBtn = document.querySelector('#prev');
+    const slides = document.querySelectorAll('.review');
+
+    index = 0;
+
+    let slidesPerView = 1;
+
+    function slideStep() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    const gap = parseFloat(getComputedStyle(track).columnGap || getComputedStyle(track).gap || 0);
+    return slideWidth + gap; // distance from one card start to the next
+    }
+
+    function updateCarousel() {
+    track.style.transform = `translateX(-${index * slideStep()}px)`;
+    }
+
+    nextBtn.addEventListener('click', () => {
+    index += slidesPerView;
+    if (index >= slides.length-2) index = 0; // loop, -2 depends on how many slides it moves with click 
+    updateCarousel();
+    });
+
+    prevBtn.addEventListener('click', () => {
+    index -= slidesPerView;
+    if (index < 0) {
+        index = Math.floor((slides.length - 3) / slidesPerView) * slidesPerView; // -3 depends on how many slides it moves with click 
+    }
+    updateCarousel();
+    });
+
+    window.addEventListener('resize', updateCarousel);
+});
